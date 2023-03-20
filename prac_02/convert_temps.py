3dfa2ce4b6d6d0412a1f9ba609ed9fff753559af
Mi Songhua <1512224016@qq.com>
2023-03-20 11:26:31 +0800

    Write convert temperature program

---
+++ prac_02/convert_temps.py
@@ -0,0 +1,21 @@
+import random
+
+
+def main():
+    filename_01 = "temps_input.txt"
+    in_file = open(filename_01, "r")
+    filename_02 = "temps_output.txt"
+    out_file = open(filename_02, "w")
+    for line in in_file:
+        temp_in_celsius = convert_fahrenheit_to_celsius(float(line))
+        print(temp_in_celsius, file=out_file)
+    in_file.close()
+    out_file.close()
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+main()
