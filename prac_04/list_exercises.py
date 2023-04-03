3ad02a024beb849ad3319d521eb9ff83fcdfae15
Mi Songhua <1512224016@qq.com>
2023-04-03 13:22:42 +0800

    Prac_04 final

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,7 +5,7 @@ for i in range(5):
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
-print(f"The smallest number is {max(numbers)}")
+print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
