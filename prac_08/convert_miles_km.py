c54588f8eff6c2c6a8f2c1262428530e9e3fb29c
Mi Songhua <1512224016@qq.com>
2023-05-08 11:52:40 +0800

    prac08 pull request

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,25 +10,25 @@ class MilesConverterApp(App):
 
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
     def build(self):
-        """Build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         self.message = "Press button to see the convert result"
         return self.root
 
     def handle_convert(self):
-        """Handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget."""
         value = self.get_valid_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
-        """Handle up/down button press, update the text input with new value, call calculation function"""
+        """Handle up/down button press, update the text input with new value, call calculation function."""
         value = self.get_valid_miles() + change
         self.root.ids.input_miles.text = str(value)
 
     def get_valid_miles(self):
-        """Get text input from text entry widget, convert to float"""
+        """Get text input from text entry widget, convert to float."""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
