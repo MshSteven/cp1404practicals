abf7aad69ffcd0e792849396721daeabe3222cea
Mi Songhua <1512224016@qq.com>
2023-05-08 11:40:48 +0800

    prac08 final

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    message = StringProperty()
+
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    def build(self):
+        """Build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Press button to see the convert result"
+        return self.root
+
+    def handle_convert(self):
+        """Handle calculation (could be button press or other call), output result to label widget """
+        value = self.get_valid_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, change):
+        """Handle up/down button press, update the text input with new value, call calculation function"""
+        value = self.get_valid_miles() + change
+        self.root.ids.input_miles.text = str(value)
+
+    def get_valid_miles(self):
+        """Get text input from text entry widget, convert to float"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+
+MilesConverterApp().run()
---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+NEW_COLOUR = (1, 1, 0, 1)  # RGBA for yellow
+
+
+class DynamicLabelsApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **msh):
+        """Construct main app."""
+        super().__init__(**msh)
+        # basic data (model) example - dictionary of names: label number
+        self.name_to_label = {"Tom": "1", "Jerry": "2", "Coco": "3"}
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels from data and add them to the GUI."""
+        for name in self.name_to_label:
+            # create a label for each data entry, specifying the text
+            temp_label = Label(text=name)
+            temp_label.color = NEW_COLOUR
+            self.root.ids.main.add_widget(temp_label)
+
+    def clear_all(self):
+        """Clear all widgets that are children of the "entries_box" layout widget."""
+        self.root.ids.main.clear_widgets()
+
+
+DynamicLabelsApp().run()
