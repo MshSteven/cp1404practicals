10ab740a36e6371272ac76ac8331ea2290f8720b
Mi Songhua <1512224016@qq.com>
2023-05-20 16:43:21 +0800

    prac09 final ver

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,6 +5,7 @@ MENU = "q)uit, c)hoose taxi, d)rive\n>>> "
 
 
 def main():
+    """Run a system to get chosen taxi and display the fare of customer."""
     current_taxi = None
     bill_to_date = 0.00
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
@@ -13,7 +14,7 @@ def main():
     print("Let's drive!")
     option = input(MENU).lower()
     while option != options[0]:
-        if option == options[1]:
+        if option == options[1]:    # This option is to display the available taxi.
             display_available_taxi(taxis, "Taxis available:")
             choice = int(input("Choose taxi: "))
             if choice < 0 or choice >= len(taxis):
@@ -21,7 +22,7 @@ def main():
             else:
                 current_taxi = choice
 
-        elif option == options[2]:
+        elif option == options[2]:  # This option is to get the drive distance and show the fare.
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
             else:
@@ -41,6 +42,7 @@ def main():
 
 
 def display_available_taxi(taxis, prompt):
+    """Display the available taxi."""
     print(prompt)
     for i, available_taxi in enumerate(taxis):
         print(f"{i} - {available_taxi}")
