e49a57e266e2aaf8b1a64bde7041400cc2f26b0a
Mi Songhua <1512224016@qq.com>
2023-05-22 11:28:08 +0800

    prac10 Compelete Testing

---
+++ prac_10/REFLECTION.md
@@ -0,0 +1,5 @@
+## CP1404 Prac10
+# MI Songhua
+1. I think I can estimate the work more accurate than before.
+2. The Pull Request is a nice way to know how my work done. It give me another side of coin to look at my work.
+3. I think I can do the commit steps more and write the comments more details.
---
+++ prac_10/test.py
@@ -0,0 +1,30 @@
+# def create_dictionary(food, calories):
+#     food_to_calories = {}
+#     for i in range(len(food)):
+#         food_to_calories[food[i]] = calories[i]
+#     # return sorted(food_to_calories.items(), key=lambda x: x[1], reverse=True)
+#     return food_to_calories
+#
+#
+# calories = [75, 65, 6175]
+# food = ["Egg", "Apple", "Carrot Cake"]
+# print(create_dictionary(food, calories))
+
+
+# def do_thing(i):
+#     return "**" * i
+#
+#
+# def main():
+#     for i in range(5):
+#         print(do_thing(i))
+#
+#
+# main()
+
+from prac_06.car import Car
+
+
+test_car = Car()
+assert test_car._odometer == 0
+print("Work")
